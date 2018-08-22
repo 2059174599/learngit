2856f1d1c2845b120e77774a00169aa4781cb39a
dexiansheng <779548320@qq.com>
2018-08-22 18:05:36 +0800

    test

--- jieba_baike/new.py
+++ jieba_baike/new.py
@@ -64,4 +64,4 @@ if __name__ == '__main__':
     spider.set_cookies()
     # content=spider.open_url(url='http://www.gsxt.gov.cn/corp-query-entprise-info-hot-search-list.html?province=100000')
     # print content
-    测试git
+    git
